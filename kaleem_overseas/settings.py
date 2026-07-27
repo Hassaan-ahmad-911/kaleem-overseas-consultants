53ec7f0f2d799b9282460c6b82048cacfed35172
Hassaan-ahmad-911 <hassaanahmad911@gmail.com>
2026-07-28 00:30:27 +0500

    Move database credentials to environment variables

--- kaleem_overseas/settings.py
+++ kaleem_overseas/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-kaleem-overseas-consultant-secret-key"
@@ -52,11 +52,11 @@ ASGI_APPLICATION = "kaleem_overseas.asgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kaleem_overseas_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Loveforall',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
